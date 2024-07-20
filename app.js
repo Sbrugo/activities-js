5c6a32caa84985f5d3958bf2092d6a244bebe87d
Sofi Brugo <brugosofi@gmail.com>
2024-07-20 11:36:10 -0300

    new changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "purple", "orange", "yellow", "blue", "lightblue", "black", "brown", "pink"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector('.color');
